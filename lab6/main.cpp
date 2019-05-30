1c24b1f47320e7d9bc373e1493fe823a5c2bc7f0
Neknu <Neknu@users.noreply.github.com>
2019-05-30 07:36:41 +0300

    task 2 is ready

--- lab6/main.cpp
+++ lab6/main.cpp
@@ -13,6 +13,7 @@ using std::cout;
 using std::endl;
 
 int const RAND = 5;
+int const MAX = 1000000;
 
 int rand_num(int max) {
     static std::random_device rd;
@@ -119,6 +120,7 @@ LinkedList* delete_element_linked_list(LinkedList* list, string data) {
         finded->next = temp->next;
         delete temp;
     }
+    list->length--;
     return list;
 }
 
@@ -145,6 +147,112 @@ LinkedList* create_random_linked_list(int count) {
     return list;
 }
 
+// task 2
+
+struct ArrayList {
+    string arr[MAX];
+    int length;
+    ArrayList() {
+        for(int i = 0; i < MAX; i++)
+            arr[i] = "";
+        length = 0;
+    }
+};
+
+ArrayList* create_array_list() {
+    ArrayList* arr = new ArrayList;
+    return arr;
+}
+
+ArrayList* shift_array_up(ArrayList* arr, int num) {
+    for(int i = arr->length - 1; i >= num; i--)
+        arr->arr[i + 1] = arr->arr[i];
+    return arr;
+}
+
+ArrayList* add_element_array_list(ArrayList* arr, string data) {
+    if(arr->length == 0)
+        arr->arr[0] = data;
+    else {
+        int i = 0;
+        while (i < arr->length && arr->arr[i] < data) {
+            i++;
+        }
+        if(i == arr->length) {
+            arr->arr[i] = data;
+        }
+        else {
+            if(arr->arr[i] == data) {
+                return arr;
+            }
+            else {
+                shift_array_up(arr, i);
+                arr->arr[i] = data;
+            }
+        }
+    }
+    arr->length++;
+    return arr;
+}
+
+int find_element_array_list(ArrayList* arr, string data) {
+    for(int i = 0; i < arr->length; i ++) {
+        if(arr->arr[i] > data)
+            return -1;
+        if (arr->arr[i] == data)
+            return i;
+    }
+    return -1;
+}
+
+int find_element_diapason_array_list(ArrayList* arr, string start, string end) {
+    for(int i = 0; i < arr->length; i ++) {
+        if(arr->arr[i] > end)
+            return -1;
+        if (start <= arr->arr[i] && arr->arr[i] <= end)
+            return i;
+    }
+    return -1;
+}
+
+ArrayList* shift_array_down(ArrayList* arr, int num) {
+    for(int i = num; i < arr->length - 1; i++)
+        arr->arr[i] = arr->arr[i + 1];
+    return arr;
+}
+
+ArrayList* delete_element_array_list(ArrayList* arr, string data) {
+    int curr = find_element_array_list(arr, data);
+    if (curr < 0)
+        return arr;
+    else {
+        shift_array_down(arr, curr);
+    }
+    arr->length--;
+    return arr;
+}
+
+void print_array_list(ArrayList* arr) {
+    cout << endl;
+    for(int i = 0; i < arr->length; i ++) {
+        cout << arr->arr[i] << " ";
+    }
+    cout << endl;
+}
+
+ArrayList* create_random_array_list(int count) {
+    ArrayList* arr = create_array_list();
+    string word;
+    for(int i = 0; i < count; i++) {
+        int rand = rand_num(RAND) + 1;
+        word = "";
+        for(int j = 0; j < rand; j++)
+            word += char(65 + rand_num(rand) + 1);
+        add_element_array_list(arr, word);
+    }
+    return arr;
+}
+
 int main() {
 
     //task 1
@@ -155,5 +263,11 @@ int main() {
 //    delete_element_linked_list(list, "B");
 //    print_linked_list(list);
 
+    ArrayList* arr = create_random_array_list(RAND);
+    //ArrayList* arr = create_array_list();
+    add_element_array_list(arr, "ABB");
+    add_element_array_list(arr, "B");
+    delete_element_array_list(arr, "B");
+    print_array_list(arr);
     return 0;
 }
--- test/main.cpp
+++ test/main.cpp
@@ -1,98 +1,184 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <random>
+#include <chrono>
+#include <functional>
+#include <algorithm>
 
-using namespace std;
+int const V = 1;
+int const COUNT = 127;
+int const KOL = 40;
+int const INF = 1000000000;
 
-void Binary_tree_of_search();
-struct Tree{
-    Tree* father;
-    Tree* l_son;
-    Tree* r_son;
-    int left_son;
-    int right_son;
-    int data;
+using std::string;
+using std::cin;
+using std::cout;
+using std::endl;
+using std::min;
+
+double rand_num(double max) {
+    static std::random_device rd;
+    static std::seed_seq seed { rd(), static_cast<unsigned int>(time(nullptr))};
+    static std::mt19937_64 gen(seed);
+    std::uniform_real_distribution<double> dist(-max, max);
+
+    return dist(gen);
+}
+
+
+struct func {
+    string name;
+    int level;
 };
 
-int main(){
-    Binary_tree_of_search();
-    return 0;
+struct element {
+    string type;
+    int id;
+    int count_func;
+    func functions[5 + V];
+    double rr;
+    double mark;
+
+    element* dad;
+    element* son;
+    element* next; //brother
+};
+
+//task one
+
+struct dstnc {
+    double distance; //distnce between 2 components
+    double first_mark;
+    double second_mark;
+    dstnc* left;
+    dstnc* right;
+};
+
+dstnc* newNode(double item) {
+    dstnc *temp =  new dstnc;
+    temp->distance = item;
+    temp->left = temp->right = nullptr;
+    return temp;
 }
 
-void recurs_add(Tree* start_el, int in_data, Tree* add_son){
-    if (start_el->data <= in_data){
-        if (start_el->right_son == 1)
-            recurs_add(start_el->r_son, in_data, add_son);
-        else if (start_el->right_son != 1){
-            start_el->r_son = add_son;
-            add_son->father = start_el;
-            start_el->right_son = 1;
-        }
+double mark(element* el) {
+    double res = 0;
+    for(int i = 0; i < el->count_func; i++) {
+        res += el->functions[i].level*el->functions[i].level;
     }
-    else if (start_el->data > in_data){
-        if (start_el->left_son == 1)
-            recurs_add(start_el->l_son, in_data, add_son);
-        else if (start_el->left_son != 1){
-            start_el->l_son = add_son;
-            add_son->father = start_el;
-            start_el->left_son = 1;
+    res = pow(res, V + 2*el->rr);
+    return res;
+}
+
+
+double dist(element* el, element* other) {
+    double res = 0;
+    for(int i = 0; i < el->count_func; i++)
+        for(int j = 0; j < other->count_func; j++)
+            if(el->functions[i].name == other->functions[j].name) {
+                res += pow(min(el->functions[i].level, other->functions[j].level), 2);
+            }
+    res += 1;
+    res = pow(res, min(el->rr, other->rr) - 2*V);
+    return res;
+}
+
+int create_distances(element* components[], struct dstnc* distances[], int count) {
+    int k = 0;
+    for(int i = 0; i < count; i++)
+        for(int j = i + 1; j < count; j++) {
+            dstnc* d = new dstnc;
+            d->distance = dist(components[i], components[j]);
+            d->first_mark = components[i]->mark;
+            d->second_mark = components[j]->mark;
+            distances[k] = d;
+            k++;
         }
+    return k;
+}
+
+element* create_random_element() {
+    element* el = new element;
+    el->type = char(75+int(rand_num(10)));
+    el->id = int(rand_num(10)) + 10;
+    el->count_func = abs(int(rand_num(5+V)));
+    for(int i = 0; i < el->count_func; i++) {
+        el->functions[i].name =char(75+int(rand_num(5)));
+        el->functions[i].level = abs(int(rand_num(1000))) + 1;
+    }
+    el->rr = abs(rand_num(1));
+    el->mark = mark(el);
+    return el;
+}
+
+
+void printBinTree(struct dstnc *root, int depth) {
+    if (root != nullptr)
+    {
+        printBinTree(root->left, depth + 1);
+        for(int i = 0; i < depth; i++)
+            cout << " --";
+        cout << root->distance << "\n";
+        printBinTree(root->right, depth + 1);
     }
 }
-void add_new_el(Tree* start_el, int data_1, int data_2){
-    Tree* new_el_1 = new Tree;
-    Tree* new_el_2 = new Tree;
-    new_el_1->data = data_1;
-    new_el_2->data = data_2;
-    if (data_2 >= data_1){
-        new_el_1->r_son = new_el_2;
-        new_el_1->right_son = 1;
-        new_el_2->father = new_el_1;
-    }
-    else if (data_2 < data_1){
-        new_el_1->l_son = new_el_2;
-        new_el_1->left_son = 1;
-        new_el_2->father = new_el_1;
-    }
-    if (data_1 < start_el->data){
-        if (start_el->left_son == 1)
-            recurs_add(start_el->l_son, data_1, new_el_1);
-        else if (start_el->left_son != 1){
-            start_el->left_son = 1;
-            start_el->l_son = new_el_1;
-            new_el_1->father = start_el;
-        }
-    }
-    else if (new_el_1->data >= start_el->data){
-        if (start_el->right_son == 1)
-            recurs_add(start_el->r_son, data_1, new_el_1);
-        else {
-            start_el->right_son = 1;
-            start_el->r_son = new_el_1;
-            new_el_1->father = start_el;
-        }
-    }
+
+
+dstnc* insert(dstnc* dstnc, double key) {
+
+    if (dstnc == nullptr) return newNode(key);
+
+    if (key < dstnc->distance)
+        dstnc->left  = insert(dstnc->left, key);
+    else if (key > dstnc->distance)
+        dstnc->right = insert(dstnc->right, key);
+
+    return dstnc;
 }
-void cout_tree(Tree* start_el, int indent){
-    if (start_el->l_son != 0){
-        for (int i = 0; i < indent; i++)
-            cout << " ";
-        cout << "|l:" << start_el->l_son->data << endl;
-        if (start_el->l_son->l_son != 0 || start_el->l_son->r_son != 0)
-            cout_tree(start_el->l_son, indent+2);
+
+dstnc* find(dstnc* dstnc, double key) {
+    while(dstnc) {
+        if(dstnc->distance == key)
+            return dstnc;
+        if(dstnc->distance > key)
+            dstnc = dstnc->right;
+        else
+        if(dstnc->distance < key)
+            dstnc = dstnc->left;
     }
-    if (start_el->r_son != 0){
-        for (int i = 0; i < indent; i++)
-            cout << " ";
-        cout << "|r:" << start_el->l_son->data << endl;
-        if (start_el->r_son->l_son != 0 || start_el->r_son->r_son != 0)
-            cout_tree(start_el->l_son, indent+2);
-    }
+    return nullptr;
 }
-void Binary_tree_of_search(){
-    Tree* Boss = new Tree;
-    Boss->father = 0;
-    Boss->data = rand()%14+4;
-    for (int i = 0; i < 8; i++)
-        add_new_el(Boss, rand()%20+1, rand()%20+1);
-    cout << Boss->data << endl;
-    cout_tree(Boss, 2);
+
+
+dstnc* build_bin_tree(dstnc* distances[], int count_dist) {
+    dstnc *root = nullptr;
+    root = insert(root, distances[0]->distance);
+    for(int i = 0; i < count_dist; i++)
+        insert(root, distances[i]->distance);
+    printBinTree(root, 1);
+    return root;
+}
+
+int main() {
+    element* components[COUNT + 1];
+
+
+
+    //task 4
+
+    dstnc* distances[KOL*KOL + 1];
+    for(int i = 0; i < KOL/4; i++) {
+       components[i] = create_random_element();
+    }
+    int count_dist = create_distances(components, distances, KOL/4);
+    dstnc* root = build_bin_tree(distances, count_dist);
+
+    dstnc* find_dist = find(root, distances[4]->distance);
+    if(find_dist)
+        cout << "this is data from finded node - " << find_dist->distance << "\n";
+    else
+       cout << "this node doesn't exist!" << "\n";
+
+    return 0;
 }
