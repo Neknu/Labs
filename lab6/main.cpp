ab4254c20ac77440ff25e12a88258f85c98bf05c
Neknu <Neknu@users.noreply.github.com>
2019-05-30 13:34:52 +0300

    task 3 is ready but with 1 bug in deleting

--- lab6/main.cpp
+++ lab6/main.cpp
@@ -12,7 +12,7 @@ using std::cin;
 using std::cout;
 using std::endl;
 
-int const RAND = 5;
+int const RAND = 10;
 int const MAX = 1000000;
 
 int rand_num(int max) {
@@ -253,6 +253,173 @@ ArrayList* create_random_array_list(int count) {
     return arr;
 }
 
+// task 3
+
+struct BinNode{
+    BinNode* left;
+    BinNode* right;
+    string data;
+};
+
+struct BinTree{
+    BinNode* root;
+    int count_of_elements;
+    BinTree(){
+        root = nullptr;
+        count_of_elements = 0;
+    }
+};
+
+BinTree* create_bin_tree() {
+    BinTree* bin = new BinTree;
+    return bin;
+}
+
+
+BinNode* newNode(string item) {
+    BinNode *temp =  new BinNode;
+    temp->data = item;
+    temp->left = temp->right = nullptr;
+    return temp;
+}
+
+
+
+BinNode* add_element_bin(BinNode* BinNode, string key) {
+
+    if (BinNode == nullptr) return newNode(key);
+
+    if (key < BinNode->data)
+        BinNode->left  = add_element_bin(BinNode->left, key);
+    else if (key > BinNode->data)
+        BinNode->right = add_element_bin(BinNode->right, key);
+    else if (key == BinNode->data)
+        return nullptr;
+
+    return BinNode;
+}
+
+BinTree* add_element_bin_tree(BinTree* bin, string key) {
+    BinNode* new_node;
+    new_node = add_element_bin(bin->root, key);
+    if(bin->root == nullptr) {
+        bin->root = new_node;
+    }
+    if (new_node) {
+        bin->count_of_elements++;
+    }
+    return bin;
+}
+
+BinNode* find_element_bin_tree(BinTree* bin, string key) {
+    BinNode* BinNode = bin->root;
+    while(BinNode) {
+        if(BinNode->data == key) {
+            return BinNode;
+        }
+        if(BinNode->data < key) {
+            BinNode = BinNode->right;
+        }
+        else {
+            if (BinNode->data > key) {
+                BinNode = BinNode->left;
+            }
+        }
+    }
+    return nullptr;
+}
+
+BinNode* find_element_diapason_bin_tree(BinTree* bin, string start, string end) {
+    BinNode* BinNode = bin->root;
+    while(BinNode) {
+        if(start <= BinNode->data && BinNode->data <= end)
+            return BinNode;
+        if(BinNode->data < end)
+            BinNode = BinNode->right;
+        else
+        if(BinNode->data > start)
+            BinNode = BinNode->left;
+    }
+    return nullptr;
+}
+
+BinTree* swap_delete_bin_node(BinTree* bin, BinNode* to_del, BinNode* temp) {
+    swap(to_del->data, temp->data);
+    to_del->left = temp->left;
+    to_del->right = temp->right;
+    delete temp;
+    return bin;
+}
+
+BinTree* delete_element_bin_tree(BinTree* bin, string data) {
+    BinNode* to_del = find_element_bin_tree(bin, data);
+    if(!to_del)
+        return bin;
+    else {
+        if(to_del->right == nullptr && to_del->left == nullptr)
+            delete to_del;
+        else {
+            if (to_del->right == nullptr) {
+                BinNode* temp = to_del->left;
+                swap_delete_bin_node(bin, to_del, temp);
+            }
+            else {
+                if(to_del->left == nullptr) {
+                    BinNode* temp = to_del->right;
+                    swap_delete_bin_node(bin, to_del, temp);
+                }
+                else {
+                    BinNode* curr = to_del->left;
+                    while(curr->right)
+                        curr = curr->right;
+                    swap(to_del->data, curr->data);
+                    if(curr->left) {
+                        BinNode *temp = curr->left;
+                        swap_delete_bin_node(bin, curr, temp);
+                    }
+                    else {
+                        delete curr;
+                    }
+                }
+            }
+        }
+    }
+    return bin;
+}
+
+BinTree* create_random_bin_tree(int count) {
+    BinTree* bin = create_bin_tree();
+    string word;
+    for(int i = 0; i < count; i++) {
+        int rand = rand_num(RAND) + 1;
+        word = "";
+        for(int j = 0; j < rand; j++)
+            word += char(65 + rand_num(rand) + 1);
+        add_element_bin_tree(bin, word);
+    }
+    return bin;
+}
+
+void print_bin(BinNode *root, int depth) {
+    if (root != nullptr)
+    {
+        print_bin(root->left, depth + 1);
+        for(int i = 0; i < depth; i++)
+            cout << " --";
+        cout << root->data << "\n";
+        print_bin(root->right, depth + 1);
+    }
+}
+
+void print_bin_tree(BinTree* bin) {
+    print_bin(bin->root, 1);
+}
+
+
+
+
+
+
 int main() {
 
     //task 1
@@ -263,11 +430,25 @@ int main() {
 //    delete_element_linked_list(list, "B");
 //    print_linked_list(list);
 
-    ArrayList* arr = create_random_array_list(RAND);
-    //ArrayList* arr = create_array_list();
-    add_element_array_list(arr, "ABB");
-    add_element_array_list(arr, "B");
-    delete_element_array_list(arr, "B");
-    print_array_list(arr);
+
+    //task 2
+//    ArrayList* arr = create_random_array_list(RAND);
+//    //ArrayList* arr = create_array_list();
+//    add_element_array_list(arr, "ABB");
+//    add_element_array_list(arr, "B");
+//    delete_element_array_list(arr, "B");
+//    print_array_list(arr);
+
+    BinTree* bin = create_random_bin_tree(RAND);
+    add_element_bin_tree(bin, "D");
+    print_bin_tree(bin);
+    BinNode* curr = find_element_bin_tree(bin, "D");
+    if(curr)
+        cout << curr->data << "\n";
+    else
+        cout << "No element!" << "\n";
+    delete_element_bin_tree(bin, "D");
+    print_bin_tree(bin);
+
     return 0;
 }
